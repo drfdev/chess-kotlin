120dedfe132405ef0e3ed7a9cbc44f27758ebc4a
drfdev <drafff@gmail.com>
2019-11-26 21:36:20 +0300

    add kotlin junit test

---
+++ src/test/kotlin/dev/drf/MainTest.kt
@@ -0,0 +1,12 @@
+package dev.drf
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class MainTest {
+    @Test
+    fun test() {
+        print("Main test: assertTrue(true)")
+        assertTrue(true)
+    }
+}
